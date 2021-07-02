e92f7dde3cd8cbae653251a85405f2c2f1f6f87e
Menzo Windhouwer <menzo@windhouwer.nl>
2021-07-02 09:30:33 +0200

    First working version :-)

--- do.sh
+++ do.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-gcsplit -q -f citation.tsv. citation.tsv '/^#/' '{*}'
+#gcsplit -q -f citation.tsv. citation.tsv '/^#/' '{*}'
+#./xsl -it:main -xsl:tsv2xml.xsl TSV=citation.tsv.02 | xmllint --format - > citation.xml
 
-xsl -it:main -xsl:js2xml.xsl JS=record.json | xmllint --format - > record.xml
+./xsl.sh -it:main -xsl:js2xml.xsl JS=record.json | xmllint --format - > record.xml
+./xsl.sh -xsl:dataverse2cmdi.xsl -s:record.xml | xmllint --format - > record.cmdi
 
-xsl -it:main -xsl:tsv2xml.xsl TSV=citation.tsv.02 | xmllint --format - > citation.xml
